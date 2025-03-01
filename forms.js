45fc8ff774d5bacfc01fd31a8daecd37dfb1be35
izh4ack <brisk.text7737@fastmail.com>
2025-02-28 22:40:50 -0300

    refatorado forms.js e inclusao do botao limpar

--- forms.js
+++ forms.js
@@ -6,20 +6,34 @@ const dados = {
      genero: document.getElementById('genero').value,
      autor: document.getElementById('autor').value,
      ano: document.getElementById('ano').value,
-     nota: document.getElementById('nota').value
+     nota: document.getElementById('nota').value,
+     resenha: document.getElementById('resenha').value
 
 }
 
 
 
-    axios.post('http://localhost:8080/posts', { dados })
+    axios.post('http://localhost:8080/posts', dados)
         .then(response => {
             console.log('Dados enviados com sucesso:', response.data);
             alert('Dados enviados com sucesso!');
+            limparFormulario();
         })
         .catch(error => {
    
             console.error('Erro ao enviar dados:', error);
             alert('Erro ao enviar dados.');
         });
+}
+
+function limparFormulario()
+{
+
+    document.getElementById('titulo').value="";
+    document.getElementById('genero').value="";
+    document.getElementById('autor').value="";
+    document.getElementById('ano').value="";
+    document.getElementById('nota').value="";
+    document.getElementById('resenha').value="";
+
 }
